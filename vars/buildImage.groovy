b6ee8b296cf2bbdf3d8c4edc7598acb62a8c207e
sanjeevk021995 <sanjeev.kush95@gmail.com>
2026-03-15 22:34:36 +0530

    updated file

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,8 @@
 def call(imageName, tag) {
-
- sh """
- /kaniko/executor \
-  --dockerfile=Dockerfile \
-  --context=$(pwd) \
-  --destination=${imageName}:${tag}
- """
-
+  sh """
+    /kaniko/executor \
+      --dockerfile=Dockerfile \
+      --context=\$(pwd) \
+      --destination=${imageName}:${tag}
+  """
 }
