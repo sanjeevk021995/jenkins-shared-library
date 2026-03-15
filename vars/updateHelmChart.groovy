8966cb1403dafe253134f0644c795e4498340921
sanjeevk021995 <sanjeev.kush95@gmail.com>
2026-03-15 17:29:46 +0530

    updated helm stage

--- vars/updateHelmChart.groovy
+++ vars/updateHelmChart.groovy
@@ -3,7 +3,9 @@ def call(chartPath, releaseName, imageRepo, imageTag) {
     echo "Deploying application using Helm"
 
     sh """
-    helm upgrade --install ${releaseName} ${chartPath} \
+    helm uninstall ${releaseName} -n demo || true
+         sleep 60
+    helm upgrade --install ${releaseName} ${chartPath}  --namespace demo \
       --set image.repository=${imageRepo} \
       --set image.tag=${imageTag}
     """
